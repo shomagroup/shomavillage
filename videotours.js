37b9a99d8dfceeed5dda861a43357f22190d6659
shomagroup <kcasana@shomagroup.com>
2023-02-13 17:41:10 -0500

    Update videotours.js

---
+++ videotours.js
@@ -0,0 +1,8 @@
+$('[vt-close]').on('click', function() {
+    $('.videotour-thing').hide();
+    $('.vt-fs-pop').removeClass('show');
+});
+
+$('.videotourwrap').on('click', function() {
+    if ($(this).is("[vt='1']"))
+});
